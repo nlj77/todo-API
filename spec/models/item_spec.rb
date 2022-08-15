a4561b6f93dd880cc70b3a48a0c9ea111c4b31d6
Nick Jones <nickjones9605@gmail.com>
2022-08-15 16:33:34 -0600

    Initial commit, creates item, todo object and tests

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  describe "associations" do
+    it { should belong_to(:todo) }
+  end
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+end
